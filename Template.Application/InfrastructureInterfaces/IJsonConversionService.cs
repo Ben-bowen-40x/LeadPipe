c92004aec59dbcd0d1149413a027e6086d34ef03
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-07-10 09:42:30 -0600

    Move AppServices to the same namespace

--- Template.Application/InfrastructureInterfaces/IJsonConversionService.cs
+++ Template.Application/InfrastructureInterfaces/IJsonConversionService.cs
@@ -1,9 +1,10 @@
-﻿using CsvHelper.Configuration;
+﻿using CSharpFunctionalExtensions;
+using CsvHelper.Configuration;
 
 namespace Template.Application.InfrastructureInterfaces;
 
 public interface IJsonConversionService
 {
-    List<T> Extract<T>(FileInfo jsonFile);
-    Dictionary<bool, FileInfo> SaveToCsv<T, TMap>(List<T> entities, FileInfo csvFile) where TMap : ClassMap<T>;
+    Result<List<T>> Extract<T>(FileInfo jsonFile);
+    Result<FileInfo> SaveToCsv<T, TMap>(Result<List<T>> entities, FileInfo csvFile) where TMap : ClassMap<T>;
 }
