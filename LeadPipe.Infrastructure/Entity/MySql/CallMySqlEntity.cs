419072ce20201d4c7084a704162bcebb44096711
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-04 12:02:01 -0700

    remove unnecessary comments

--- LeadPipe.Infrastructure/Entity/MySql/CallMySqlEntity.cs
+++ LeadPipe.Infrastructure/Entity/MySql/CallMySqlEntity.cs
@@ -3,10 +3,11 @@
 public class CallMySqlEntity
 {
 #pragma warning disable IDE1006 // Naming Styles
-    public long call_id { get; set; }//c
-    public int duration { get; set; }//c
-    public string? sale_billable { get; set; }//c
-    public string? contact_number_clean { get; set; }//c
-    public DateTime called_at_utc { get; set; }//c
+    public long call_id { get; set; }
+    public int duration { get; set; }
+    public string? sale_billable { get; set; }
+    public string? contact_number_clean { get; set; }
+    public DateTime called_at_utc { get; set; }
+
 #pragma warning restore IDE1006 // Naming Styles
 }
