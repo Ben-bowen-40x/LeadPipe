5dfd12357f5a9dcd41ed6dc2727617062229cb44
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-08 11:57:00 -0700

    Update FileRWManager.cs

--- LeadPipe.Application/Manager/FileRWManager.cs
+++ LeadPipe.Application/Manager/FileRWManager.cs
@@ -12,14 +12,14 @@ public interface IFileRWManager
 public sealed class FileRWManager(IFileRWService service) : IFileRWManager
 {
     private readonly IFileRWService _service = service;
-    private const string error = "File extension not supported: ";
+    private const string errMsg = "File extension not supported: ";
     public Result<List<T>> ManageRead<T>(FileInfo file)
     {
         return file.Extension switch
         {
             ".csv" => _service.ExtractCsv<T>(file),
             ".json" => _service.ExtractJson<T>(file),
-            _ => Result.Failure<List<T>>(error + file.Extension)
+            _ => Result.Failure<List<T>>(errMsg + file.Extension)
         };
     }
     public Result ManageWrite<T>(FileInfo file, List<T> data)
@@ -28,7 +28,7 @@ public sealed class FileRWManager(IFileRWService service) : IFileRWManager
         {
             ".csv" => _service.SaveToCsv<T>(data, file),
             ".json" => _service.SaveToJson<T>(data, file),
-            _ => Result.Failure(error + file.Extension)
+            _ => Result.Failure(errMsg + file.Extension)
         };
     }
 }
