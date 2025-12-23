a8963e90a24029570f26bd93e38a1a77b12e704c
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2025-12-23 13:34:26 -0700

    Get rid of old mysqlentity persistence models

--- LeadPipe.Infrastructure/Data/Persistence/CallEntityPersistence.cs
+++ LeadPipe.Infrastructure/Data/Persistence/CallEntityPersistence.cs
@@ -1,7 +1,25 @@
-﻿using LeadPipe.Infrastructure.Entity.Sqlite;
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Translate;
 
 namespace LeadPipe.Infrastructure.Data.Persistence;
 
-internal class CallEntityPersistence(ICallRepository repo) : Persistence<ICallRepository, CallEntity>(repo), IDataPersistence<CallEntity>{ }
+internal sealed class CallEntityPersistence(ICallRepository repo) : Persistence<ICallRepository, CallEntity>(repo), IDataPersistence<CallEntity> { }
+
+internal sealed class CallPersistence(
+    IDataPersistence<CallEntity> persist,
+    IVoToEntity<Call, CallEntity> voToE
+    ) : IDataPersistence<Call>
+{
+    private readonly IDataPersistence<CallEntity> _persist = persist;
+    private readonly IVoToEntity<Call, CallEntity> _voToE = voToE;
+    public async Task<Result> SaveAsync(List<Call> t)
+    {
+        List<CallEntity> entities = [.. t.Select(_voToE.Translate)];
+        Result result = await _persist.SaveAsync(entities);
+        return result;
+    }
+}
