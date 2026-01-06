030b10aade94c08e8f9e9d3c12082a542b1dfd8d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 09:35:42 -0700

    Create YellerReportHelper.cs

---
+++ LeadPipe.Translation/Translate/EntityToReport/YellerReportHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace LeadPipe.Translation.Translate.EntityToReport;
+
+internal sealed class YellerReportHelper
+{
+    internal const string Currency = "USD";
+    internal const string Country = "us";
+}
