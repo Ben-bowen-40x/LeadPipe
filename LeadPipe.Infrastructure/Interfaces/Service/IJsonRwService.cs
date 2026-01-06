78dd170ede97df3054e504851a87fb0077536d5f
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 11:21:06 -0700

    Add an async-like write method

--- LeadPipe.Infrastructure/Interfaces/Service/IJsonRwService.cs
+++ LeadPipe.Infrastructure/Interfaces/Service/IJsonRwService.cs
@@ -6,4 +6,5 @@ public interface IJsonRwService
 {
     Result<List<T>> ReadFile<T>(FileInfo path);
     Result WriteToFile<T>(FileInfo path, List<T> items);
+    Task<Result> WriteToFileAsync<T>(FileInfo path, List<T> items);
 }
