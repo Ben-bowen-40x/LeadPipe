552bec45aa5b9d4feb59f76a9cd535e019e806d6
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-06 12:21:58 -0700

    Create ScheduleKeyAttribute.cs

---
+++ LeadPipe.Infrastructure/Attributes/ScheduleKeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace LeadPipe.Infrastructure.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+internal sealed class ScheduleKeyAttribute(Schedule key) : Attribute, IScheduleKeyAttribute
+{
+    public Schedule Key { get; } = key;
+}
+
+internal interface IScheduleKeyAttribute
+{
+    public Schedule Key { get; }
+}
