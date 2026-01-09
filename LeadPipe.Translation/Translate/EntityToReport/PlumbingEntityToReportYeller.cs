bfdc60020f3afab2e551a53b2edf4b9e80ce7260
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-09 10:46:45 -0700

    Add PlumbingEntityToReportYeller.cs

---
+++ LeadPipe.Translation/Translate/EntityToReport/PlumbingEntityToReportYeller.cs
@@ -0,0 +1,36 @@
+﻿using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Translation.Translate.EntityToReport;
+
+internal sealed class PlumbingEntityToReportYeller : IEntityToReport<PlumbingEntity, ReportYeller>
+{
+    public ReportYeller Translate(PlumbingEntity data)
+    {
+        long eventtime = data.UnixDate;
+        string eventName = "lead";
+        string num = YellerReportHelper.HashSha256(data.PhoneNumber.ToString());
+        string eventid = data.Id.ToString();
+        
+        UserData user = new()
+        {
+            ph = [num]
+        };
+        CustomData custom = new()
+        {
+            currency = YellerReportHelper.Currency,
+            value = 0
+        };
+
+        ReportYeller result = new()
+        {
+            event_id = eventid,
+            event_name = eventName,
+            event_time = eventtime,
+            custom_data = custom,
+            user_data = user
+        };
+        return result;
+    }
+}
