b5c164df844a7a4280d7b498d94a1e3ad626ccef
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-01-26 16:48:05 -0700

    Update CustardMySqlEntity.cs

--- LeadPipe.Infrastructure/Entity/MySql/CustardMySqlEntity.cs
+++ LeadPipe.Infrastructure/Entity/MySql/CustardMySqlEntity.cs
@@ -7,8 +7,8 @@ public class CustardMySqlEntity
     public int status { get; set; }
     public string? phone1 { get; set; }
     public string? phone2 { get; set; }
-    public DateTime dateAdded { get; set; }
-    public DateTime dateCancelled { get; set; }
+    public DateTime? dateAdded { get; set; }
+    public DateTime? dateCancelled { get; set; }
 
     public ICollection<SandMySqlEntity> subscriptions { get; set; } = [];
 #pragma warning restore IDE1006 // Naming Styles
