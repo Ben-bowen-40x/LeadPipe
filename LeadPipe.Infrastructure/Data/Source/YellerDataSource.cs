f7bce5dc8e8330b3739a5a3010ba58b4fd262b6f
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-02 18:46:25 -0700

    Update YellerDataSource.cs

--- LeadPipe.Infrastructure/Data/Source/YellerDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/YellerDataSource.cs
@@ -1,16 +1,13 @@
 ﻿using CSharpFunctionalExtensions;
-using LeadPipe.Domain.ValueObjects;
 using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Service;
-using LeadPipe.Infrastructure.Interfaces.Translate;
 
 namespace LeadPipe.Infrastructure.Data.Source;
 
-public class YellerDataSource(IYellerService yeller, IVoToDto<Plumbing, YellerDto> voToDto) : IDataSourceAsync<YellerDto>
+public class YellerDataSource(IYellerService yeller) : IDataSourceAsync<YellerDto>
 {
     private readonly IYellerService _yeller = yeller;
-    private readonly IVoToDto<Plumbing, YellerDto> _voToDto = voToDto;
 
     public async Task<Result<List<YellerDto>>> LoadAsync()
     {
