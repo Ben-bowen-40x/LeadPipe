1d9da14af2500c5cc7c23119f0787cecbd4e6b0c
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-09 12:11:28 -0700

    remove unnecessary files

--- LeadPipe.Infrastructure/Interfaces/Service/IFirstTouchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using LeadPipe.Infrastructure.Interfaces.Core;
-
-namespace LeadPipe.Infrastructure.Interfaces.Service;
-
-internal interface IFirstTouchService
-{
-    TLink? GetFirstTouch<TLink>(IEnumerable<TLink> links)
-        where TLink : IEntity, IHasUnixMatchDate;
-}
