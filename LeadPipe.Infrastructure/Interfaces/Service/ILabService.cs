8a6103b5add123a08379ea4b930c384d34b560fd
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-10 13:54:31 -0700

    Update ILabService.cs

--- LeadPipe.Infrastructure/Interfaces/Service/ILabService.cs
+++ LeadPipe.Infrastructure/Interfaces/Service/ILabService.cs
@@ -5,6 +5,6 @@ namespace LeadPipe.Infrastructure.Interfaces.Service;
 
 public interface ILabService
 {
-    Task<Result<List<Plumbing>>> GetLabsAsync(int errorLimit = 5);
-    Task<Result<List<Plumbing>>> UpdateDataAsync(int errorLimit = 5);
+    Task<Result<List<Plumbing>>> GetLabsAsync(int errorLimit = 5, CancellationToken ct = default);
+    Task<Result<List<Plumbing>>> UpdateDataAsync(int errorLimit = 5, CancellationToken ct = default);
 }
