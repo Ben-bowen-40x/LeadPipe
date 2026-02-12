d1c0a08ab76422738a8439ab8197f43b1407c69d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 13:36:29 -0700

    Update ISyncStateRepository.cs

--- LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/ISyncStateRepository.cs
+++ LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/ISyncStateRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
 using LeadPipe.Infrastructure.Entity.Sqlite;
 
 namespace LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
@@ -6,6 +7,6 @@ namespace LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
 public interface ISyncStateRepository
 {
     Task<Result<List<SyncStateEntity>>> UpsertRangeAsync(List<SyncStateEntity> entities);
+    Task<Result<SyncStateEntity>> GetByKeyAsync(Source? source, SyncKey key);
     Task<Result<SyncStateEntity>> GetByIdAsync(BusinessId id);
 }
-
