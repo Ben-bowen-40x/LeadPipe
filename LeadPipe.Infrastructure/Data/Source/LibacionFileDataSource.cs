d430c04fd50a810f713eb6c191c57943fc3dbe2d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 16:12:40 -0700

    Update LibacionFileDataSource.cs

--- LeadPipe.Infrastructure/Data/Source/LibacionFileDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/LibacionFileDataSource.cs
@@ -1,4 +1,6 @@
-﻿using LeadPipe.Infrastructure.Dto;
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Service;
 using LeadPipe.Infrastructure.Settings;
@@ -8,4 +10,10 @@ namespace LeadPipe.Infrastructure.Data.Source;
 
 internal class LibacionFileDataSource(IInfrastructureSettings settings, ICsvRwService csv, IJsonRwService json, ILogger<LibacionFileDataSource> logging)
     : FileDataSource<LibacionDto, LibacionFileDataSource>(new FileInfo(settings.LibacionSourceLoc!), csv, json, logging), IDataSourceAsync<LibacionDto>
-{ }
+{
+    protected override Result<List<LibacionDto>> FlattenInvalid(Result<List<LibacionDto>> fileContents)
+    {
+        if (fileContents.IsFailure) return fileContents;
+        return fileContents.Value.Where(v => PhoneNumber.TryParse(v.PhoneNumber, out var _)).ToList();
+    }
+}
