33ae2afc257eb9761f261acfdbe2bd09264690ca
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 13:38:37 -0700

    Update ISyncSettings.cs

--- LeadPipe.Infrastructure/Settings/ISyncSettings.cs
+++ LeadPipe.Infrastructure/Settings/ISyncSettings.cs
@@ -5,10 +5,11 @@ public interface ISyncSettings
     double DefaultInterval { get; set; }
     double DefaultSourceInterval { get; set; }
     double CalliInterval { get; set; }
-    double LabInterval {get;set;}
-    double LeafInterval {get;set;}
-    double LeasedInterval {get;set;}
-    double LibacionInterval {get;set;}
-    double PanInterval {get;set;}
-    double YellerInterval {get;set;}
+    double LabInterval { get; set; }
+    double LeafInterval { get; set; }
+    double LeasedInterval { get; set; }
+    double LibacionInterval { get; set; }
+    double PanInterval { get; set; }
+    double YellerInterval { get; set; }
+    double LatherInterval { get; set; }
 }
