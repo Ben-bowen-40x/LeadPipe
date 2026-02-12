8139b69f04fc290ced58f28c45fbc0c7acb4a07e
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 16:13:17 -0700

    Add LatherUpdateService.cs

---
+++ LeadPipe.Infrastructure/Service/Update/LatherUpdateService.cs
@@ -0,0 +1,18 @@
+﻿using LeadPipe.Application.Service;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Attributes;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Service.Update;
+
+[SourceKey(Source.Lather)]
+internal sealed class LatherUpdateService(
+    IDataSourceAsync<LatherDto> source,
+    IDtoToVo<LatherDto, Plumbing> dtoToVo,
+    IVoToEntity<Plumbing, PlumbingEntity> voToEntity,
+    IDataPersistence<PlumbingEntity> persistence
+) : UpdateService<LatherDto, Plumbing, PlumbingEntity>(source, dtoToVo, voToEntity, persistence), IUpdateService<Plumbing>
+{ }
