e4e61f7512c1c364ada1590b4ee841d6233c5f61
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-12 16:13:04 -0700

    Add LatherDto.cs

---
+++ LeadPipe.Infrastructure/Dto/LatherDto.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace LeadPipe.Infrastructure.Dto;
+
+public class LatherDto
+{
+    public string? Phone { get; set; }
+    public string? Date { get; set; }
+    public string? Time { get; set; }
+    [Name("Time Zone")]
+    public string? TimeZone { get; set; }
+    [Name("LeadID")]
+    public string? LeadId { get; set; }
+}
