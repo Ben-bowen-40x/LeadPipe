ef9f78ccf624b56d6f6e9945e208adea34dc1da5
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-13 11:54:09 -0700

    Add CornFormulaPersistence.cs

---
+++ LeadPipe.Infrastructure/Data/Persistence/CornFormulaPersistence.cs
@@ -0,0 +1,12 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+
+namespace LeadPipe.Infrastructure.Data.Persistence;
+
+internal sealed class CornFormulaPersistence(
+    IDataPersistence<CornEntity> persist,
+    IVoToEntity<CornFormula, CornEntity> voToE
+) : VoPersistence<CornEntity, CornFormula>(persist, voToE), IDataPersistence<CornFormula>
+{ }
