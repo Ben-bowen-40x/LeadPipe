83e3c38ecae5bc4c2bb1ae624fd113ca6b6ab62d
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-19 12:01:18 -0700

    Add unique constraint to plumbing

--- LeadPipe.Infrastructure.Sqlite/Context/PlumbingContext.cs
+++ LeadPipe.Infrastructure.Sqlite/Context/PlumbingContext.cs
@@ -123,7 +123,14 @@ public sealed class PlumbingContext(DbContextOptions<PlumbingContext> options) :
             .ToTable(TableNames.PlumbingEntitiesName);
         plumb.HasKey(p => p.Id);
         plumb.Property(p => p.Id).ValueGeneratedOnAdd(); // Internal Id
-        plumb.HasIndex(p => new { p.PhoneNumber, p.Date, p.Source });
+        plumb.HasIndex(p => new 
+            {
+                p.PhoneNumber,
+                p.UnixDate,
+                p.Source,
+                p.MetaData
+            }
+        ).IsUnique();
         plumb.HasIndex(p => p.Date);
         plumb.Property(p => p.Source)
             .HasConversion<string>()
