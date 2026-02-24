525ebce563eca54caba1f9b5aae2d251be0a98c1
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-24 14:21:53 -0700

    Add ICatManSettings.cs

---
+++ LeadPipe.Infrastructure/Settings/ICatManSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace LeadPipe.Infrastructure.Settings;
+
+public interface ICatManSettings
+{
+    string? CatManClientName { get; set; }
+    string? CatToken { get; set; }
+    string? CatManDateFormat { get; set; }
+    string? CatBaseEndpoint { get; set; }
+    string? CatAccountId { get; set; }
+}
