a87768a4be9116776c623cc896455d897f6ad5e9
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-02-26 12:03:06 -0700

    Create ICatManService.cs

---
+++ LeadPipe.Infrastructure/Interfaces/Service/ICatManService.cs
@@ -0,0 +1,9 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Dto;
+
+namespace LeadPipe.Infrastructure.Interfaces.Service;
+
+public interface ICatManService
+{
+    Task<Result<List<CatmanDto>>> GetAllAsync(DateTime start, DateTime end);
+}
