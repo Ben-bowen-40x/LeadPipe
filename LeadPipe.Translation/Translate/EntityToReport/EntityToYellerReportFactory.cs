1e1882d1d311ea57a4ea5d4042019e15172db0aa
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-02 13:18:43 -0700

    Add EntityToYellerReportFactory.cs

---
+++ LeadPipe.Translation/Translate/EntityToReport/EntityToYellerReportFactory.cs
@@ -0,0 +1,14 @@
+﻿using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadPipe.Translation.Translate.EntityToReport;
+
+internal sealed class EntityToYellerReportFactory(IServiceProvider provider) : IEntityToYellerReportFactory
+{
+    private readonly IServiceProvider _provider = provider;
+    public IEntityToReport<TEntity, ReportYeller> GetService<TEntity>()
+    {
+        return _provider.GetRequiredService<IEntityToReport<TEntity, ReportYeller>>();
+    }
+}
