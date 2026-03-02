8c4fdecf58f4416718675cd1f406953b02777ac1
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-02 11:34:14 -0700

    Add CatManManager.cs

---
+++ LeadPipe.Application/Manager/CatManManager.cs
@@ -0,0 +1,21 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Application.Service;
+using LeadPipe.Domain.ValueObjects;
+
+namespace LeadPipe.Application.Manager;
+
+public interface ICatManManager
+{
+    Task<Result> Manage(bool refresh);
+}
+internal class CatManManager(IUpdateFactory factory) : ICatManManager
+{
+    private readonly IUpdateService<Caliper> _catMan = factory.GetService<Caliper>(Source.Yeller);
+    public async Task<Result> Manage(bool refresh)
+    {
+        var result = refresh
+            ? await _catMan.UpdateDataAsync(false)
+            : await _catMan.GetDataAsync(false);
+        return result;
+    }
+}
