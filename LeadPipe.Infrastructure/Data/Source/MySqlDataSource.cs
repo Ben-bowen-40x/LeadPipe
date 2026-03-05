9cafba88d613c9b49e4befe7c7374e8c4a4ba3f0
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-05 15:55:08 -0700

    Add MySqlDataSource.cs

---
+++ LeadPipe.Infrastructure/Data/Source/MySqlDataSource.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+
+namespace LeadPipe.Infrastructure.Data.Source;
+
+public class MySqlDataSource
+{
+    protected static async Task<Result> SyncStateAsync(ISyncStateRepository sync, DateTimeOffset dateUpdated, SyncKey key)
+    {
+        SyncStateEntity state = new()
+        {
+            BusinessId = BusinessId.From(key.Value),
+            LastSyncUtc = dateUpdated.UtcDateTime,
+            UnixLastSyncUtc = dateUpdated.ToUnixTimeSeconds()
+        };
+
+        Result<List<SyncStateEntity>> upsert = await sync.UpsertRangeAsync([state]);
+
+        return upsert;
+    }
+
+    protected static async Task<DateTimeOffset> LatestSyncDate(ISyncStateRepository sync, DateTimeOffset allowableDate, SyncKey key)
+    {
+        Result<SyncStateEntity> state = await sync.GetByKeyAsync(null, key);
+        DateTimeOffset syncDate = state.IsFailure
+            ? allowableDate
+            : DateTimeOffset.FromUnixTimeSeconds(state.Value.UnixLastSyncUtc);
+
+        return syncDate;
+    }
+
+}
