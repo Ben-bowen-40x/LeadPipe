589878759adacd19db0e11d1ed61fbdc657ced0e
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-12 15:59:51 -0600

    Update CaliperMySqlDataSource.cs

--- LeadPipe.Infrastructure/Data/Source/CaliperMySqlDataSource.cs
+++ LeadPipe.Infrastructure/Data/Source/CaliperMySqlDataSource.cs
@@ -1,6 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
 using LeadPipe.Infrastructure.Entity.MySql;
-using LeadPipe.Infrastructure.Entity.Sqlite;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Repository.MySql;
 using LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
@@ -9,28 +9,52 @@ namespace LeadPipe.Infrastructure.Data.Source;
 
 public sealed class CaliperMySqlDataSource(
     ICaliperMySqlRepository repo,
-    IRepositoryFactory factory
-    ) : IDataSourceAsync<CaliperMySqlEntity>
+    ISyncStateRepository sync
+    ) : MySqlDataSource(sync), IDataSourceAsync<CaliperMySqlEntity>
 {
     private readonly ICaliperMySqlRepository _repo = repo;
-    private readonly IRepository<CaliperEntity> _caliper = factory.GetRepository<CaliperEntity>();
+    private readonly Result<DateTimeOffset>? _latest;
+
+    private async Task<Result<DateTimeOffset>> Latest() => _latest ?? await LatestSyncDate(SyncKey.Caliper);
     public async Task<Result<List<CaliperMySqlEntity>>> LoadAsync(bool withDetails)
     {
+        // Retrieve all calipers since jan 1 last year
         DateTime dateFilter = new(DateTime.UtcNow.Year - 1, 1, 1);
         Result<List<CaliperMySqlEntity>> found = await _repo.FindAsync(c => c.called_at_utc >= dateFilter, withDetails);
+
+        // Update latest sync date
+        Result<DateTimeOffset> latest = await Latest();
+        DateTimeOffset latestValue = latest.IsFailure
+            ? DateTimeOffset.UtcNow.AddDays(-7) // This will only be used if no values are found in the db AND latest sync returns no date
+            : latest.Value;
+        DateTimeOffset updatedLatest = NewLatest(found, latestValue);
+        await SyncStateAsync(updatedLatest, SyncKey.Caliper);
+
         return found;
     }
 
     public async Task<Result<List<CaliperMySqlEntity>>> RefreshAsync(bool withDetails)
     {
-        // Find the most recent caliper date in the caliper repository
-        Result<List<CaliperEntity>> calipersResult = await _caliper.GetAllAsync();
-        if (calipersResult.IsFailure || calipersResult.Value.Count == 0)
+        // Find the most recent caliper date from syncstate
+        Result<DateTimeOffset> latest = await Latest();
+        if (latest.IsFailure)
             return await LoadAsync(withDetails);
 
-        DateOnly mostRecent = DateOnly.FromDateTime(calipersResult.Value.Max(m => m.Date));
-        DateTime mostRecentDate = new DateTime(mostRecent, new TimeOnly(0)) - TimeSpan.FromDays(7);
+        // Retrieve calipers on or after the latest date
+        DateTime mostRecentDate = latest.Value.UtcDateTime.AddDays(-7);
         Result<List<CaliperMySqlEntity>> found = await _repo.FindAsync(c => c.called_at_utc >= mostRecentDate, withDetails);
+
+        // Save new syncstate
+        DateTimeOffset updatedLatest = NewLatest(found, mostRecentDate);
+
+        await SyncStateAsync(updatedLatest, SyncKey.Caliper);
+
         return found;
     }
+
+    private static DateTimeOffset NewLatest(Result<List<CaliperMySqlEntity>> found, DateTimeOffset defaultLatest)
+        => found.IsSuccess && found.Value.Count > 0
+            ? found.Value.Select(v => new DateTimeOffset(v.called_at_utc, TimeSpan.Zero)).Max()
+            : defaultLatest;
+
 }
