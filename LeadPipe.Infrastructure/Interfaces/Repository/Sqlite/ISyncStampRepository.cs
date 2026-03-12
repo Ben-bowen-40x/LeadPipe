ae0b8e3706c7f9b56c775cff676293cb934a72d4
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-12 13:29:37 -0600

    Update ISyncStampRepository.cs

---
+++ LeadPipe.Infrastructure/Interfaces/Repository/Sqlite/ISyncStampRepository.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Entity.Sqlite;
+
+namespace LeadPipe.Infrastructure.Interfaces.Repository.Sqlite;
+
+public interface ISyncStampRepository
+{
+    Task<Result<SyncStampEntity>> GetByKeyAsync(Source? source, SyncKey key);
+    Task<Result<SyncStampEntity>> UpsertAsync(SyncStampEntity entity);
+}
