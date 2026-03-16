8732af6a6f3f94706644ec7b6a52216950d41fee
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-16 17:13:38 -0600

    Add SystemClock.cs

---
+++ LeadPipe.Infrastructure/Service/SystemClock.cs
@@ -0,0 +1,5 @@
+﻿using LeadPipe.Infrastructure.Interfaces.Core;
+
+namespace LeadPipe.Infrastructure.Service;
+
+internal class SystemClock : IClock { public DateTimeOffset UtcNow => DateTimeOffset.UtcNow; }
