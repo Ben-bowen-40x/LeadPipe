73c0cd74e13c81cfda1f2030d23a4693c0f266ff
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-23 12:29:02 -0600

    Add AttributionResultToReportYellerOld.cs

---
+++ LeadPipe.Translation/Translate/EntityToReport/AttributionResultToReportYellerOld.cs
@@ -0,0 +1,48 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Dto;
+using LeadPipe.Infrastructure.Entity;
+using LeadPipe.Infrastructure.Interfaces.Translate;
+using LeadPipe.Infrastructure.Settings;
+
+namespace LeadPipe.Translation.Translate.EntityToReport;
+
+internal sealed class AttributionResultToReportYellerOld(IYellerSettings settings) : IEntityToReport<AttributionResult, ReportYeller_Old>
+{
+    private readonly string _action = settings.YellerActionSource!;
+    public ReportYeller_Old Translate(AttributionResult result)
+    {
+        var custard = result.Custard;
+
+        long eventtime = result.FirstTouchUnixDate;
+        string eventname = "purchase";
+        string eventid = custard.Id.ToString();
+
+        string num1 = YellerReportHelper.HashSha256(
+            custard.PhoneNumber.Number.ToString());
+
+        string n2 = custard.PhoneNumber2 is null
+            ? PhoneNumber.Default.ToString()
+            : custard.PhoneNumber2.Number.ToString();
+
+        string num2 = YellerReportHelper.HashSha256(n2);
+
+        UserData user = new() { ph = [num1, num2] };
+
+        CustomData custom = new()
+        {
+            currency = YellerReportHelper.Currency,
+            value = result.Value
+        };
+
+        return new ReportYeller_Old
+        {
+            event_id = eventid,
+            action_source = _action,
+            event_name = eventname,
+            event_time = eventtime,
+            custom_data = custom,
+            user_data = user
+        };
+    }
+
+}
