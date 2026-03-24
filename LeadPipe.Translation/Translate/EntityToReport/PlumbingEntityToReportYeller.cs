2f5178ac53e6f745e01a15199f102aea9a40ccbe
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-24 09:16:11 -0600

    Update PlumbingEntityToReportYeller.cs

--- LeadPipe.Translation/Translate/EntityToReport/PlumbingEntityToReportYeller.cs
+++ LeadPipe.Translation/Translate/EntityToReport/PlumbingEntityToReportYeller.cs
@@ -1,36 +1,15 @@
-﻿using LeadPipe.Infrastructure.Dto;
-using LeadPipe.Infrastructure.Entity.Sqlite;
-using LeadPipe.Infrastructure.Interfaces.Translate;
+﻿using LeadPipe.Infrastructure.Entity.Sqlite;
 using LeadPipe.Infrastructure.Settings;
 
 namespace LeadPipe.Translation.Translate.EntityToReport;
 
-internal sealed class PlumbingEntityToReportYeller(IYellerSettings settings) : IEntityToReport<PlumbingEntity, ReportYeller>
+internal sealed class PlumbingEntityToReportYeller(IYellerSettings settings) : EntityToReportYeller<PlumbingEntity>
 {
-    private readonly string _action = settings.YellerActionSource!;
-    public ReportYeller Translate(PlumbingEntity data)
-    {
-        long eventtime = data.UnixDate;
-        string eventName = "lead";
-        string num = YellerReportHelper.HashSha256(data.PhoneNumber.Number.ToString());
-        string eventid = data.Id.ToString() + "-161221"; // 16 == p, 12 == l, 21 == u
+    protected override string Type => settings.YellerPlumbingName!;
 
-        UserData user = new() { ph = [num] };
-        CustomData custom = new()
-        {
-            currency = YellerReportHelper.Currency,
-            value = 0
-        };
+    protected override long GetEntityId(PlumbingEntity e) => e.Id;
 
-        ReportYeller result = new()
-        {
-            event_id = eventid,
-            event_name = eventName,
-            event_time = eventtime,
-            action_source = _action,
-            custom_data = custom,
-            user_data = user
-        };
-        return result;
-    }
+    protected override long GetPhoneNumber(PlumbingEntity e) => e.PhoneNumber.Number;
+
+    protected override long GetUnixDate(PlumbingEntity e) => e.UnixDate;
 }
