4c055790bd81e36b475fc6018a3e4241af173d72
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-03-26 13:01:30 -0600

    Add PlumbingPhoneNumber.cs

---
+++ LeadPipe.Infrastructure/Entity/Sqlite/PlumbingPhoneNumber.cs
@@ -0,0 +1,25 @@
+﻿using LeadPipe.Domain.ValueObjects;
+using LeadPipe.Infrastructure.Interfaces.Core;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LeadPipe.Infrastructure.Entity.Sqlite;
+
+public class PlumbingPhoneNumber : IEntity
+{
+    public PlumbingPhoneNumber() { }
+    [SetsRequiredMembers]
+    private PlumbingPhoneNumber(PlumbingPhoneNumber p)
+    {
+        Id = p.Id;
+        PhoneNumber = p.PhoneNumber;
+        PlumbingId = p.PlumbingId;
+        Plumbing = p.Plumbing;
+    }
+
+    public long Id { get; set; }
+    public required PhoneNumber PhoneNumber { get; set; }
+    public required long PlumbingId { get; set; }
+    public PlumbingEntity? Plumbing { get; set; }
+
+    internal PlumbingPhoneNumber Clone() => new(this);
+}
