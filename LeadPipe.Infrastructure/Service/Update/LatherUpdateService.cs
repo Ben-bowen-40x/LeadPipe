7163eaa3e093e0b5c9763e6b4101f68b9aadd620
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-02 16:13:14 -0600

    Update LatherUpdateService.cs

--- LeadPipe.Infrastructure/Service/Update/LatherUpdateService.cs
+++ LeadPipe.Infrastructure/Service/Update/LatherUpdateService.cs
@@ -14,5 +14,5 @@ internal sealed class LatherUpdateService(
     IDtoToVo<LatherDto, Plumbing> dtoToVo,
     IVoToEntity<Plumbing, PlumbingEntity> voToEntity,
     IDataPersistence<PlumbingEntity> persistence
-) : UpdateService<LatherDto, Plumbing, PlumbingEntity>(source, dtoToVo, voToEntity, persistence), IUpdateService<Plumbing>
+) : UpdateService<LatherDto, Plumbing, PlumbingEntity>(source, dtoToVo, voToEntity, persistence, SyncKey.Plumbing), IUpdateService<Plumbing>
 { }
