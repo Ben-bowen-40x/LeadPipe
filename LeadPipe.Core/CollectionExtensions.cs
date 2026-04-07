5ba2a146b9603a273a2405d52ef70b74aeac3634
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-07 08:58:18 -0600

    Add CollectionExtensions.cs

---
+++ LeadPipe.Core/CollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace LeadPipe.Core;
+
+public static class CollectionExtensions
+{
+    public static HashSet<TResult> ToHashSetFast<TSource, TResult>(
+        this ICollection<TSource> source,
+        Func<TSource, TResult> selector)
+    {
+        HashSet<TResult> result = new(source.Count);
+        foreach (var item in source)
+            result.Add(selector(item));
+
+        return result;
+    }
+    public static Dictionary<TKey, TValue> ToDictionaryFast<TSource, TKey, TValue>(
+        this ICollection<TSource> source,
+        Func<TSource, TKey> key,
+        Func<TSource, TValue> value) where TKey : notnull
+    {
+        Dictionary<TKey, TValue> result = new(source.Count);
+        foreach (var item in source)
+            result.Add(key(item), value(item));
+
+        return result;
+    }
+}
