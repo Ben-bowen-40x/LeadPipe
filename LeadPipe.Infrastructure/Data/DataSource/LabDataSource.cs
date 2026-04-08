d76c011b3dc4f7e211852cb4e6702bb81b6f2118
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-08 13:24:19 -0600

    Update LabDataSource.cs

--- LeadPipe.Infrastructure/Data/DataSource/LabDataSource.cs
+++ LeadPipe.Infrastructure/Data/DataSource/LabDataSource.cs
@@ -1,31 +1,26 @@
 ﻿using CSharpFunctionalExtensions;
-using LeadPipe.Domain.ValueObjects;
 using LeadPipe.Infrastructure.Dto;
 using LeadPipe.Infrastructure.Interfaces.Core;
 using LeadPipe.Infrastructure.Interfaces.Service;
-using LeadPipe.Infrastructure.Interfaces.Translate;
 
 namespace LeadPipe.Infrastructure.Data.DataSource;
 
-public class LabDataSource(ILabService lab, IVoToDto<Plumbing, LabDto> voToDto) : IDataSourceAsync<LabDto>
+public class LabDataSource(ILabService lab) : IDataSourceAsync<LabDto>
 {
     private readonly ILabService _lab = lab;
-    private readonly IVoToDto<Plumbing, LabDto> _voToDto = voToDto;
     public async Task<Result<List<LabDto>>> LoadAsync(bool _ = false)
     {
-        Result<List<Plumbing>> get = await _lab.GetLabsAsync();
+        Result<List<LabDto>> get = await _lab.GetLabsAsync();
         if (get.IsFailure)
             return Result.Failure<List<LabDto>>(get.Error);
-        List<LabDto> result = [.. get.Value.Select(_voToDto.Translate)];
-        return result;
+        return get;
     }
 
     public async Task<Result<List<LabDto>>> RefreshAsync(bool _ = false)
     {
-        Result<List<Plumbing>> get = await _lab.UpdateDataAsync();
+        Result<List<LabDto>> get = await _lab.UpdateDataAsync();
         if (get.IsFailure)
             return Result.Failure<List<LabDto>>(get.Error);
-        List<LabDto> result = [.. get.Value.Select(_voToDto.Translate)];
-        return result;
+        return get;
     }
 }
