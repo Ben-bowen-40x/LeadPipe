19cb7c45a0d9dea070b909ed1d6a2af86a0bb3f6
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-09 15:31:59 -0600

    Rename Token.cs to TokenDto.cs

--- LeadPipe.Infrastructure/Dto/Token.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LeadPipe.Infrastructure.Dto;
-
-public class Token
-{
-    public string Access_token { get; set; } = null!;
-    public string Token_type { get; set; } = null!;
-    public string? Refresh_token { get; set; }
-    public int Expires_in { get; set; }
-}
---
+++ LeadPipe.Infrastructure/Dto/TokenDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LeadPipe.Infrastructure.Dto;
+
+public class TokenDto
+{
+    [JsonPropertyName("access_token")]
+    public string AccessToken { get; set; } = null!;
+    [JsonPropertyName("token_type")]
+    public string TokenType { get; set; } = null!;
+    [JsonPropertyName("refresh_token")]
+    public string? RefreshToken { get; set; }
+    [JsonPropertyName("expires_in")]
+    public int ExpiresIn { get; set; }
+}
