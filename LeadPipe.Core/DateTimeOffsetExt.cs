adc3209e614d477c9c8f8a534685fc9852cb6038
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-04-28 11:46:25 -0600

    Add DateTimeOffsetExt.cs

---
+++ LeadPipe.Core/DateTimeOffsetExt.cs
@@ -0,0 +1,13 @@
+﻿namespace LeadPipe.Core;
+
+public static class DateTimeOffsetExt
+{
+    public static long ToUnixTime(this DateTimeOffset dto)
+    {
+        return dto.ToUnixTimeMilliseconds();
+    }
+    public static DateTimeOffset FromUnixTime(this long l)
+    {
+        return DateTimeOffset.FromUnixTimeMilliseconds(l);
+    }
+}
