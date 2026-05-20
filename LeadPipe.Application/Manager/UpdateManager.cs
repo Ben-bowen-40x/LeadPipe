6eab30458d5d85c50e7d6c2711af54a9cc35f9df
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-05-19 18:35:31 -0600

    Update UpdateManager.cs

--- LeadPipe.Application/Manager/UpdateManager.cs
+++ LeadPipe.Application/Manager/UpdateManager.cs
@@ -11,13 +11,11 @@ public interface IUpdateManager
 public record ForceRunRefresh(bool ForceRun, bool Refresh);
 internal sealed class UpdateManager(
     ISourceDataUpdateManager sourceData,
-    ICoreDataUpdateManager coreData,
-    IAssociationManager associate
+    ICoreDataUpdateManager coreData
     ) : IUpdateManager
 {
     readonly ISourceDataUpdateManager _source = sourceData;
     readonly ICoreDataUpdateManager _core = coreData;
-    readonly IAssociationManager _associate = associate;
     public async Task<Result> Manage(Source source, ForceRunRefresh frr)
     {
         var core = await _core.Manage(frr);
