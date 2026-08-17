9660e7dd82ae7b48615bb08757f9f0333c54a219
WeCodeAwayYourProblems <90477602+WeCodeAwayYourProblems@users.noreply.github.com>
2026-08-17 11:15:17 -0600

    Added SheetUploadService.cs and IUploadService.cs

---
+++ LeadPipe.Infrastructure/Service/SheetUploadService.cs
@@ -0,0 +1,12 @@
+﻿using CSharpFunctionalExtensions;
+using LeadPipe.Infrastructure.Interface.Service;
+
+namespace LeadPipe.Infrastructure.Service;
+
+internal class SheetUploadService : IUploadService
+{
+    public Result UploadData<Row>(IList<Row> data, string sheetId, string tabName)
+    {
+        return Result.Failure("Not Implemented");
+    }
+}
